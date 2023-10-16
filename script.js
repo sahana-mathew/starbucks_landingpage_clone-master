9d72214414b8d4a9365933c30dd85b6ca1699981
sahana-mathew <130448873+sahana-mathew@users.noreply.github.com>
2023-10-16 15:41:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function imgSlider(anything) {
+    document.querySelector(".starbucks").src = anything;
+  }
+  
+  function changeCircleColor(color) {
+    const circle = document.querySelector(".circle");
+    circle.style.background = color;
+  }
+  
+  function changeColor(button) {
+    const textbox = document.querySelector(".button");
+    textbox.style.background = button;
+  }
+
+  function changetext(color) {
+    const span = document.querySelector(".span");
+    span.style.color = color;
+  }
+
+  
